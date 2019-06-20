6eca3c13960d8c7a8c5c4d3255b6f58cb271e743
freddneos <fredd@neosdev.com.br>
2019-06-20 00:13:52 -0300

    degub-tag-sucks

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ void main() async {
   runApp(
     MaterialApp(
       home: Home(),
+      debugShowCheckedModeBanner:false,
       theme: ThemeData(
         hintColor: Colors.amber,
         primaryColor: Colors.white,
